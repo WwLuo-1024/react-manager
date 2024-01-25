d746934be34be4cc6e8ec0d7db240b81d3b992a3
WwLuo-1024 <abrahamwong1408@gmail.com>
2024-01-25 18:05:22 +0000

    feat: runtime environment encapsulation

---
+++ src/config/index.ts
@@ -0,0 +1,52 @@
+/**
+ * 环境配置封装
+ */
+
+type ENV = "dev" | "stg" | "prd";
+
+let env: ENV = "dev";
+
+env = document.documentElement.dataset.env as ENV;
+console.log("env:", env);
+
+if (location.host.indexOf("localhost") > -1) {
+  env = "dev";
+} else if (location.host === "driver-stg.marsview.cc") {
+  env = "stg";
+} else {
+  env = "prd";
+}
+
+const config = {
+  dev: {
+    baseApi: "/api",
+    uploadApi: "http://api-driver.marsview.cc",
+    cdn: "http://xxx.aliyun.com",
+    mock: true,
+    mockApi:
+      "https://www.fastmock.site/mock/5841b82d5672783b6fd62bb2a06aeb1f/api",
+  },
+
+  stg: {
+    baseApi: "/api",
+    uploadApi: "http://api-driver.marsview.cc",
+    cdn: "http://xxx.aliyun.com",
+    mock: false,
+    mockApi:
+      "https://www.fastmock.site/mock/5841b82d5672783b6fd62bb2a06aeb1f/api",
+  },
+
+  prd: {
+    baseApi: "/api",
+    uploadApi: "http://api-driver.marsview.cc",
+    cdn: "http://xxx.aliyun.com",
+    mock: false,
+    mockApi:
+      "https://www.fastmock.site/mock/5841b82d5672783b6fd62bb2a06aeb1f/api",
+  },
+};
+
+export default {
+  env,
+  ...config[env],
+};
